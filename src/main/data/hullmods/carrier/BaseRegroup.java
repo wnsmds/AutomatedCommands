102e6160e76970f18bc3503ff35a0778f78c385d
borgrel <borgrel@aol.com>
2023-06-04 23:02:51 +0200

    Refactored Carrier Regroup

--- src/main/data/hullmods/carrier/BaseRegroup.java
+++ src/main/data/hullmods/carrier/BaseRegroup.java
@@ -4,18 +4,19 @@ import com.fs.starfarer.api.GameState;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.*;
 import data.hullmods.AutomatedHullMod;
-import data.hullmods.Util;
+import data.combatlog.Util;
 
 import java.text.MessageFormat;
-import java.util.Map;
 
 public class BaseRegroup extends AutomatedHullMod {
+    private static final String REGROUP = Global.getSettings().getString(Util.MOD_KEY, "REGROUP");
+    private static final String ENGAGE = Global.getSettings().getString(Util.MOD_KEY, "ENGAGE");
+    private static final MessageFormat REGROUP_INAPPLICABLE = Util.resolveSubstitutions(Util.MOD_KEY + ":REGROUP_INAPPLICABLE");
+
+    private static final String NO_WINGS = "NO_WINGS";
+
     private static final float THRESHOLD = 0.8f;
     private static final String THRESHOLD_TEXT = Util.percentToString(THRESHOLD);
-
-    private static final String REGROUP = Global.getSettings().getString(Util.MOD_KEY,"REGROUP");
-    private static final String ENGAGE = Global.getSettings().getString(Util.MOD_KEY,"ENGAGE");
-    private static final MessageFormat REGROUP_INAPPLICABLE = Util.resolveSubstitutions(Util.MOD_KEY +":REGROUP_INAPPLICABLE");
 
     private final float limit;
     private final String limitText;
@@ -24,11 +25,10 @@ public class BaseRegroup extends AutomatedHullMod {
         limit = 0.2f;
         limitText = Util.percentToString(limit);
     }
+
     @Override
     public boolean isApplicableToShip(ShipAPI ship) {
-        if (!ship.hasLaunchBays()) return false;
-        //return !shipHasOtherModInCategory(ship, personality.id, CATEGORY);
-        return true;
+        return ship.hasLaunchBays();
     }
 
     @Override
@@ -36,48 +36,53 @@ public class BaseRegroup extends AutomatedHullMod {
         return REGROUP_INAPPLICABLE.format(new Object[]{ship});
     }
 
-    //TODO make invalid without wings
+    private String generateTag(ShipAPI ship) {
+        return ship.getId() + "_regroup" + limitText;
+    }
+
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
         if (Global.getCurrentState() != GameState.COMBAT) return;
 
+        float rate = calculateReplacementRate(ship);
+        if (rate > 1.0f) return; //there are no fighter wings installed in the carrier
+
+        if (rate > THRESHOLD) {
+            ship.setPullBackFighters(false);
+        } else if (rate < limit) {
+            ship.setPullBackFighters(true);
+        }
+    }
+
+    private float calculateReplacementRate(ShipAPI ship) {
         float rate = 0.0f;
         int count = 0;
 
-        for (FighterLaunchBayAPI bay: ship.getLaunchBaysCopy()) {
+        for (FighterLaunchBayAPI bay : ship.getLaunchBaysCopy()) {
             if (bay.getWing() == null) continue;
             rate += bay.getCurrRate();
             count++;
         }
-        //Assume count is not 0 because hullmod is invalid without wings
-        //TODO add an ignore flag for carriers with the mod that have no installed wings
-        if (count == 0) return;
-        rate/=count;
 
-        String tag = ship.getId() + "_regroup" + limitText;
-        CombatEngineAPI engine = Global.getCombatEngine();
-        Map<String, Object> customData = engine.getCustomData();
-        if (customData.containsKey(tag)) {
-            if (rate > THRESHOLD) {
-                customData.remove(tag);
-                ship.setPullBackFighters(false);
-            }
-        } else {
-            if (rate < limit) {
-                customData.put(tag,tag);
-                ship.setPullBackFighters(true);
-            }
+        if (count == 0) {
+            return 2.0f; //there are no fighter wings installed in the carrier
         }
+        return rate / count;
     }
 
     @Override
     public String getDescriptionParam(int index, ShipAPI.HullSize hullSize) {
         switch (index) {
-            case 0: return REGROUP;
-            case 1: return limitText;
-            case 2: return ENGAGE;
-            case 3: return THRESHOLD_TEXT;
-            default: return null;
+            case 0:
+                return REGROUP;
+            case 1:
+                return limitText;
+            case 2:
+                return ENGAGE;
+            case 3:
+                return THRESHOLD_TEXT;
+            default:
+                return null;
         }
     }
 }
